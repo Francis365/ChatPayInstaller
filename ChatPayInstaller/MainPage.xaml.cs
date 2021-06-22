e079963c69771a433c897248eb59f26ec0c3d8b7
dolapobabs <dolapob@gmail.com>
2021-06-22 21:55:37 +0100

    build

---
+++ ChatPayInstaller/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatPay.AppInstallHelper;
+using Xamarin.Forms;
+
+namespace ChatPayInstaller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            AskForPermission();
+        }
+        private async void AskForPermission()
+        {
+
+            var res = ChatPay.AppInstallHelper.InstallationHelper.IsChatPayInstalled();
+            bool allowed = await ChatPay.AppInstallHelper.InstallationHelper.AskForRequiredPermission();
+            if (!allowed)
+            {
+                await DisplayAlert("Alert", "You should allow the permission for download apk", "Ok");
+            }
+            else
+            {
+                //await DisplayAlert("Congra!", "You have permission to do the next step", "OK");
+            }
+        }
+        private async void EnableKeyBoardBtn_Clicked(object sender, EventArgs e)
+        {
+            ChatPay.AppInstallHelper.InstallationHelper.ShowKeyboardSettings();
+
+        }
+        private async void ForceUpdateBtn_Clicked(object sender, EventArgs e)
+        {
+            ChatPay.AppInstallHelper.InstallationHelper.ChangeKeyboard();
+        }
+        private async void LaunchBtn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var userdetails = new UserDetails()
+                {
+                    SMUsername = "segzwealth",
+                    appversion = 0,
+                    LoginMethod = "Password",
+                    SMPassword = "Jehovah1$",
+                    SMToken = "6670",
+                    logDetails = new LogDetails() { DeviceIMEI = "web", DeviceOS = "web", HardwareIMEI = "Web", IPAddress = "" }
+                };
+                //ChatPay.AppInstallHelper.InstallationHelper.ToggleKeyboard();
+                var res = await ChatPay.AppInstallHelper.InstallationHelper.LaunchChatPay(userdetails);
+                if (res)
+                {
+                    await Task.Delay(2000);
+                    ChatPay.AppInstallHelper.InstallationHelper.ToggleKeyboard();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Alert", ex.Message, "Ok");
+            }
+
+        }
+
+        private async void downloadBtn_Clicked(object sender, EventArgs e)
+        {
+            bool allowed = await ChatPay.AppInstallHelper.InstallationHelper.AskForRequiredPermission();
+            if (!allowed)
+            {
+                await DisplayAlert("Alert", "You should allow the permission for download apk", "Ok");
+            }
+            else
+            {
+                //await DisplayAlert("Congra!", "You have permission to do the next step", "OK");
+                var res = await ChatPay.AppInstallHelper.InstallationHelper.InstallChatPayEmbedded();
+
+                // ChatPay.AppInstallHelper.InstallationHelper.InstallApp("375380948", ChatPay.AppInstallHelper.InstallMode.AppStore);
+                //await Task.Run(() => InstallAppAutomatically());
+            }
+
+
+        }
+        private void AskForPermissionBtn_Clicked(object sender, EventArgs e)
+        {
+            AskForPermission();
+        }
+        private async void uninstallBtn_Clicked(object sender, EventArgs e)
+        {
+            ChatPay.AppInstallHelper.InstallationHelper.UninstallChatPay();
+
+        }
+    }
+}
