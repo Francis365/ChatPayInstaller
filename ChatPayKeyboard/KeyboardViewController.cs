4726d9a7878bc61df3c0f53d17263761a0a42bc4
Francis365 <anyeji.francis@gmail.com>
2021-06-25 04:08:36 +0100

    ReAdded extension, Implemented Dependency Service for login

---
+++ ChatPayKeyboard/KeyboardViewController.cs
@@ -0,0 +1,83 @@
+﻿using Binding;
+using Foundation;
+
+using ObjCRuntime;
+using System;
+using UIKit;
+
+namespace ChatPayKeyboard
+{
+    public partial class KeyboardViewController : UIInputViewController
+    {
+
+        public KeyboardViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void UpdateViewConstraints()
+        {
+            base.UpdateViewConstraints();
+
+            // Add custom view sizing constraints here
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        class callback : KeyboardControllerCallback
+        {
+
+            KeyboardViewController controller;
+
+            public callback(KeyboardViewController controller)
+            {
+                this.controller = controller;
+            }
+            public override void OnKeyPadPressedWithText(string text)
+            {
+                controller.TextDocumentProxy.InsertText(text);
+
+            }
+
+            public override void TextLeftOfCusorWithText(string text)
+            {
+
+            }
+
+            public override void TextRightOfCusorWithText(string text)
+            {
+
+            }
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            //show view from binding library
+            var proxy = new KeyboardProxy();
+
+            proxy.OnloginWithViewController(this, new callback(this));
+        }
+
+        public override void TextWillChange(IUITextInput textInput)
+        {
+            // The app is about to change the document's contents. Perform any preparation here.
+        }
+
+        public override void TextDidChange(IUITextInput textInput)
+        {
+
+        }
+    }
+}
---
+++ ChatPayKeyboard/Services/AuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ChatPay.AppInstallHelper;
+using ChatPayInstaller.Interfaces;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(KeyboardExtension.Services.AuthenticationService))]
+namespace KeyboardExtension.Services
+{
+    public class AuthenticationService : IAuthService
+    {
+        public void AuthenticateUser(UserDetails userDetails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
